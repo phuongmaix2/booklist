633bde15ac206f921b13119c7bc051b6b046376c
Mai Phuong Luu <luuphuongmai2803@gmail.com>
2026-09-15 14:57:53 +0300

    add book and delete

--- src/main/java/hw2/booklist/domain/Book.java
+++ src/main/java/hw2/booklist/domain/Book.java
@@ -14,6 +14,15 @@ public class Book {
     private String author;
     private int publicationYear;
     private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     private double price;
 
     public Book() {
--- src/main/java/hw2/booklist/web/BookController.java
+++ src/main/java/hw2/booklist/web/BookController.java
@@ -2,12 +2,15 @@ package hw2.booklist.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import hw2.booklist.domain.BookRepository;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hw2.booklist.domain.Book;
 
@@ -22,6 +25,24 @@ public class BookController {
         System.out.println(books);
         model.addAttribute("books", books);
         return "booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
     }
 
 }
